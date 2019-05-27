0927ad71117cc45040b26252eafd5fc226753bb1
CNsixgod <1245953602h@gmail.com>
2019-05-27 20:39:03 +0800

    1

---
+++ music/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <m-head></m-head>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MHead from './components/m-header/m-header'
+import Tab from './components/tab/tab'
+export default {
+  components: {
+    MHead,
+    Tab,
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
